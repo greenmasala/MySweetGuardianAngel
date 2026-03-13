f5ac2ffd8646487adcafcb5afe579c0771915372
greenmasala <sinchai2549@gmail.com>
2026-03-13 21:41:44 +0700

    Player Controller Finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,21 +9,28 @@ public class Player : MonoBehaviour
 {
     Rigidbody rb;
     int turns = 3;
+    Vector2 mouse;
+    public float MouseSen = 0.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Time.timeScale = 0.2f;
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        CameraRotation();
+
         Time.fixedDeltaTime = 0.02F * Time.timeScale;
 
         if (Input.GetKeyDown(KeyCode.Space) & turns != 0)
         {
-            rb.AddForce(new Vector3(0f, 1f, 1f) * 10, ForceMode.Impulse);
+            rb.linearVelocity = Vector3.zero;
+            rb.AddForce((Vector3.up + transform.forward) * 10, ForceMode.Impulse);
             turns--;
             Debug.Log("turns remaining: " + turns);
         }
@@ -34,6 +43,19 @@ public class Player : MonoBehaviour
         {
             Time.timeScale = 1f;
         }
+    }
+
+    void CameraRotation()
+    {
+        mouse.x += Input.GetAxis("Mouse X") * MouseSen;
+        mouse.y += Input.GetAxis("Mouse Y") * MouseSen;
+        var rotation = Quaternion.Euler(Mathf.Clamp(-mouse.y, -75, 75), mouse.x, 0);
+        rb.MoveRotation(rotation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
     }
 }
 
